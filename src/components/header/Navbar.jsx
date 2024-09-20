1a9757551262d977fd5262d1f61e2492c2284b57
harsh <harshstudy29@gmail.com>
2024-09-21 02:33:16 +0530

    making navbar responsive

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
 import ContentWrapper from '../contentWrapper/ContentWrapper'
 import './style.css';
 
 const Navbar = () => {
-    // style = {
-    //     listStyle : {
-    //         display = "flex"
-    //     }
-    // }
-    
+    const [menu, setMenu] = useState(false);
     const [nav, setNav] = useState('top');
     const [scroll, setScroll] = useState(0);
+
     const scrollHandler = () => {
+        if(menu) {
+            setMenu(false);
+        }
         const currScroll = window.scrollY;
         if (currScroll > 0) {
             setNav(currScroll > scroll ? 'hide' : 'show');
@@ -28,22 +29,27 @@ const Navbar = () => {
             window.removeEventListener("scroll", scrollHandler);
         }
     }, [scroll]);
-    
+
 
     return (
-        <header className={`header fixed text-[color:#fdeed1] w-full h-16 z-10 flex items-center transition-all duration-300 ease-out ${nav} `}>
+        <header className={`header fixed  w-full h-16 z-10 flex items-center transition-all duration-300 ease-out ${nav} `}>
             <ContentWrapper className="flex items-center justify-between">
-                <ul className={`nav-list md:flex items-center gap-14 list-none `}>
-                    <li className='nav-list-ele'><a href="#">Home</a></li>
-                    <li className='nav-list-ele'><a href="#">About</a></li>
+                <ul className={`nav-list md:flex items-center gap-14 list-none ${menu ? 'md:hidden mbView' : 'hidden'} `}>
+                    <li className={`nav-list-ele ${menu ? 'mbView-Ele' : ''}`}><a href="#">Home</a></li>
+                    <li className={`nav-list-ele ${menu ? 'mbView-Ele' : ''}`}><a href="#">About</a></li>
                 </ul>
                 <div className="logo">
                     LOGO
                 </div>
-                <ul className={`nav-list md:flex items-center gap-14 list-none `}>    
-                    <li className='nav-list-ele'><a href='#'>Contact Us</a></li>
-                    <li className='nav-list-ele'><a href='#'>Product</a></li>
+                <ul className={`nav-list md:flex items-center gap-14 list-none ${menu ? 'md:hidden mbView left-28' : 'hidden'} `}>
+                    <li className={`nav-list-ele ${menu ? 'mbView-Ele' : ''}`}><a href='#'>Contact Us</a></li>
+                    <li className={`nav-list-ele ${menu ? 'mbView-Ele' : ''}`}><a href='#'>Product</a></li>
                 </ul>
+
+                <div className="mobileView align-middle md:hidden">
+                    {menu ? <VscChromeClose className='text-white' onClick={() => setMenu(false)} /> : <SlMenu className='text-white' onClick={() => setMenu(true)} />}
+                </div>
+
             </ContentWrapper>
         </header>
     )
