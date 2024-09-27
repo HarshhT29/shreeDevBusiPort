cc05734309b76ea66e9b792672ced48dc1ce82e3
Jayesh1512 <jayeshshetestudy@gmail.com>
2024-09-28 03:20:20 +0530

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from './components/About/About'
 import Category from './components/category/Category'
 import Navbar from './components/header/Navbar'
 import Landing1 from './components/Landing/Landing1'
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Navbar />
       <Landing1 />
+      <About />
       <Category />
     </>
   )
---
+++ src/components/About/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div className="min-h-screen bg-[#f4f0ea] px-40 flex gap-20 text-[#271212] items-center justify-between text-right">
+      <img src="/img/About/img1.png" alt="" className="w-1/3" />
+      <div>
+        <h1 className="text-4xl font-bold ">Pure Copper...Pure Soul...</h1>
+        <p className="max-w-xl text-xl font-semibold">
+        Crafting excellence in copper for over 40 years with a diverse range of top-quality products designed to meet global standards at affordable prices.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
