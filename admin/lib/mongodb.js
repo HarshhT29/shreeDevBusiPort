ee83bb66350eddf1d2fd9e34a0bcdcf6a93a6752
Jayesh1512 <jayeshshetestudy@gmail.com>
2024-10-14 02:24:50 +0530

    admin ready

--- admin/lib/mongodb.js
+++ admin/lib/mongodb.js
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI; // Ensure you set this in your .env.local file
+const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
