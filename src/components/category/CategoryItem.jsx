a6c8c509c0631cc577ecfdc6d74d2f032ec4ef41
Harsh <harshstudy29@gmail.com>
2024-10-13 17:31:41 +0530

    product route from category

--- src/components/category/CategoryItem.jsx
+++ src/components/category/CategoryItem.jsx
@@ -1,6 +1,8 @@
 import { FaArrowRightLong } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 const CategoryItem = ({ title, description, image }) => {
+    const navigate = useNavigate();
     return (
         <div className="w-[80vw] sm:w-[50vw] h-[60vh] flex-shrink-0 flex flex-col sm:flex-row bg-blend-normal rounded-lg">
             <img 
@@ -11,7 +13,8 @@ const CategoryItem = ({ title, description, image }) => {
             <div className="flex flex-col justify-center p-6 sm:w-1/2">
                 <h2 className="text-3xl font-bold text-gray-800">{title}</h2>
                 <p className="mt-4 text-gray-600">{description}</p>
-                <button className="mt-6 py-2 w-1/2 bg-[#271212] text-white font-semibold rounded-full hover:bg-[#3e1e1e] flex items-center justify-center gap-2 group">
+                <button className="mt-6 py-2 w-1/2 bg-[#271212] text-white font-semibold rounded-full hover:bg-[#3e1e1e] flex items-center justify-center gap-2 group"
+                onClick={() => navigate("/product")}>
                     Get 
                     <span className="transition-transform duration-300 transform group-hover:translate-x-2">
                         <FaArrowRightLong />
